00dd6184f4909a08ccfdcbfe8a89c1fb5c8f2d09
Nicholas Nelson <maximo_bartola@hotmail.com>
2014-05-10 15:45:18 -0400

    Prototypes and Readme
    Added run_analysis.R that contains prototypes for the functions that
    I'll probably use for the final project.

---
+++ run_analysis.R
@@ -0,0 +1,44 @@
+
+
+#This script will:
+#1. Merge the training and the test sets to create one data set.
+#2. Extract only the measurements on the mean and standard deviation for each measurement. 
+#3. Use descriptive activity names to name the activities in the data set
+#4. Appropriately labels the data set with descriptive activity names. 
+#5. Create a second, independent tidy data set with the average of each variable for each activity and each subject. 
+
+
+# This function creates a data table.
+merge_test_train_sets = function(directory = .) 
+{
+    dt = DT();
+
+
+
+
+    dt
+}
+
+# This function modifies an existing data table.
+extract_mean_sd = function(dt)
+{
+}
+
+# This function modifies an existing data table.
+label_activities = function(dt)
+{
+}
+
+# This function modifies an existing data table.
+label_activies = function(dt)
+{
+}
+
+# This function returns a new data table.
+create_tidy_summary = function(dt)
+{
+    new_dt = DT();
+
+    new_dt
+}
+
